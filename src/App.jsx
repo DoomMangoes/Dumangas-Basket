4c4f86144a9a09f828cadb67a5ee6f92c388c884
Doom Mangoes <marcdionis.dumangas.20@usjr.edu.ph>
2022-11-09 23:44:39 +0800

    Working version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import List from "./List";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")));
+  } else {
+    return [];
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [isEditing,setIsEditing] = useState(false);
+  const [list, setList] = useState(getLocalStorage());
+  const [selectID,setSelectID] = useState(null);
+  const [alert,setAlert] = useState({state: false, type: "", content: ""});
+  const [check, setCheck] = useState(false);
 
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert({state: false, type: "", content: ""});
+    }, 3000);
+  
+    return () => clearTimeout(timeout);
+  }, [check]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+    
+
+    if(input && !isEditing){
+
+      setAlert({state: true, type: "confirm", content: "Item added to the list"});
+      const newItem = { id: new Date().getTime().toString(), title: input};
+      setList([...list, newItem]);
+      setInput("");
+     
+    }if(input && isEditing){
+
+      setList(
+        list.map((item) => {
+          if (item.id === selectID) {
+            return { ...item, title: input };
+          }
+          return item;
+        })
+      );
+
+      setIsEditing(false);
+      setSelectID(null);
+      setInput("");
+
+      setAlert({state: true, type: "confirm", content: "Value edited"});
+    }else if(!input){
+      setAlert({state: true, type: "warning", content: "Please enter value"});
+    }
+    
+    setCheck(!check);
+  }
+
+  const clearList = () =>{
+    setAlert({state: true, type: "warning", content: "Full list is cleared"});
+    setList([]);
+    setCheck(!check);
+  }
+
+  const deleteItem = (id) => {
+    setAlert({state: true, type: "warning", content: "Item removed"});
+    setList(list.filter((item) => item.id !== id));
+    setCheck(!check);
+  };
+
+  const editItem = (id) => {
+    const selected = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setSelectID(id);
+    setInput(selected.title);
+    console.log(selectID);
+  };
+ 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  
+      <div className="basket-box">
+       
+       {alert.state && (
+      <div className={`alert-box-${alert.type}`}>
+       {alert.content}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+       )}
+      
+
+       <div className="basket-form-box">
+
+        <form className="list-form" onSubmit={handleSubmit}>
+        <h4>Shopper's Hopper</h4>
+          <input className="item-input" type="text" placeholder="e.g eggs" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button type="submit" className="submit-btn">
+          {isEditing ? "Edit" : "Submit"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </form>
+     
+       </div>
+
+       {list.length > 0 && (
+          <div className="list-menu-box">
+        
+          <List list={list} deleteItem={deleteItem} editItem={editItem}/>
+
+          <button className="clear-all-btn" onClick={clearList}>
+          Clear All
+          </button>
+          </div>
+         )
+         
+        }
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
   )
 }
 
---
+++ src/List.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaTrash, FaEdit } from "react-icons/fa";
+
+export default function List({list,deleteItem, editItem}){
+return(
+    <div className="grocery-list-box">
+    {list.map((item)=>
+    <div key={item.id} className="grocery-item">
+    <p>{item.title}</p>
+
+    <div className="item-button-box">
+            <button className="edit-btn" onClick={() => editItem(item.id)} >
+                <FaEdit />
+            </button>
+            <button className="delete-btn" onClick={() => deleteItem(item.id)} >
+               <FaTrash />
+            </button>
+    </div>
+    </div>)
+   }
+   </div>
+);
+};
